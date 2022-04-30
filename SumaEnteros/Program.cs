f7caac4170c0fe18f396977c002b55ebb536904a
Nelson Ernesto De La Quintana <ndelaquintana@hotmail.com>
2022-04-30 10:27:34 +0200

    Agregar archivos de proyecto.

---
+++ SumaEnteros/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        var sumaEnteros = new SumaEnteros();
+        Console.WriteLine(sumaEnteros.Sume(1, 2, 3));
+
+        var listaEnteros = new List<int> { 1, 2, 3, 4 };
+        Console.WriteLine(sumaEnteros.Sume(listaEnteros));
+    }
+}
+
+class SumaEnteros
+{
+    public int Sume(params int[] enteros)
+    {
+        if (enteros.Length == 0)
+            throw new ArgumentException();
+
+        return enteros.Sum();
+    }
+    public int Sume(List<int> enteros)
+    {
+        return Sume(enteros.ToArray());
+    }
+}
